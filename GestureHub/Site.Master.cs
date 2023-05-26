66ff76b6d848db066927a8fbbd93af3a33da04c2
ZacharyAng <zachary.ang02@gmail.com>
2023-05-26 12:59:30 +0800

    Fixes

--- GestureHub/Site.Master.cs
+++ GestureHub/Site.Master.cs
@@ -7,11 +7,10 @@ using System.Web.UI.WebControls;
 
 namespace GestureHub
 {
-    public partial class SiteMaster : MasterPage
+    public partial class Site : System.Web.UI.MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
         }
     }
 }
