285196b6c4e070b6f41669658394cdc1619426ba
Kenta <jp.kenta.itakura@gmail.com>
2023-05-29 23:45:17 +0800

    Images

---
+++ .sql
@@ -0,0 +1,2 @@
+﻿INSERT INTO users (user_id, username, email, password, first_name, last_name, age, gender, created_at, updated_at, user_role)
+VALUES (1, 'exampleUser', 'example@user.com', 'examplePassword', 'Example', 'User', 25, 'M', GETDATE(), GETDATE(), 'admin');
