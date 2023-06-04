4a8d397faaf1f997344688fe0e5d9029bb61ac86
ANGELINE TEE XUE QI <Angelineteexq@gmail.com>
2023-06-04 21:33:39 +0800

    homepage

--- GestureHub/Home.aspx.cs
+++ GestureHub/Home.aspx.cs
@@ -11,7 +11,18 @@ namespace GestureHub
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            //get panel of easy courses
+            Panel easyCoursePanel = CourseC.DisplayCoursesByDifficulty("easy", userRole);
+            //insert the panel
+            EasyCoursePanelHolder.Controls.Add(easyCoursePanel);
+            //get panel of intermediate courses
+            Panel intermediateCoursePanel = CourseC.DisplayCoursesByDifficulty("intermediate", userRole);
+            //insert the panel
+            IntermediateCoursePanelHolder.Controls.Add(intermediateCoursePanel);
+            //get panel of hard courses
+            Panel hardCoursePanel = CourseC.DisplayCoursesByDifficulty("difficult", userRole);
+            //insert the panel
+            HardCoursePanelHolder.Controls.Add(hardCoursePanel);
         }
     }
 }
